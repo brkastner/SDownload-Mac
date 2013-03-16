e9b5050cb8a385ad09f4301387d7e6ff57e443de
Christian Nilsen <CNilsen9@Christians-MacBook-Air.local>
2013-03-16 13:18:11 -0500

    Initial Commit

---
+++ SDownload/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SDownload
+//
+//  Created by Christian Nilsen on 3/16/13.
+//  Copyright (c) 2013 Brennan Kastner. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ SDownload/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  SDownload
+//
+//  Created by Christian Nilsen on 3/16/13.
+//  Copyright (c) 2013 Brennan Kastner. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
